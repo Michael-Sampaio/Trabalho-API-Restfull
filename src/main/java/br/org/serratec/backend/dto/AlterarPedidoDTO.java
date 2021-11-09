af7bb19036ef4e014bec573633fa095525e6a8f6
camillaslp <slpcamilla@gmail.com>
2021-11-09 14:31:50 -0300

    Atualização dto

---
+++ src/main/java/br/org/serratec/backend/dto/AlterarPedidoDTO.java
@@ -0,0 +1,95 @@
+package br.org.serratec.backend.dto;
+
+import java.time.LocalDate;
+
+import javax.persistence.Embeddable;
+
+import br.org.serratec.backend.model.Cliente;
+import br.org.serratec.backend.model.Pedido;
+import br.org.serratec.backend.model.Status;
+
+@Embeddable
+public class AlterarPedidoDTO {
+	
+	
+		
+	private LocalDate dataPedido;
+	
+	private LocalDate dataEntrega;
+	
+	private LocalDate dataEnvio;
+	
+	private Status status;
+	
+	private Cliente cliente;
+
+	
+	public AlterarPedidoDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public AlterarPedidoDTO(Pedido pedido) {
+		
+		this.dataPedido = pedido.getDataPedido();
+		this.dataEntrega = pedido.getDataEntrega();
+		this.dataEnvio = pedido.getDataEnvio();
+		this.status = pedido.getStatus();
+		this.cliente = pedido.getCliente();
+	}
+
+
+	
+
+
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+
+
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+
+	public LocalDate getDataEnvio() {
+		return dataEnvio;
+	}
+
+
+	public void setDataEnvio(LocalDate dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+
+	public Status getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+}
---
+++ src/main/java/br/org/serratec/backend/dto/InserirPedidoDTO.java
@@ -0,0 +1,104 @@
+package br.org.serratec.backend.dto;
+
+import java.time.LocalDate;
+
+import javax.persistence.Embeddable;
+
+import br.org.serratec.backend.model.Cliente;
+import br.org.serratec.backend.model.Pedido;
+import br.org.serratec.backend.model.Status;
+
+@Embeddable
+public class InserirPedidoDTO {
+	
+	
+	private Long id;
+	
+	private LocalDate dataPedido;
+	
+	private LocalDate dataEntrega;
+	
+	private LocalDate dataEnvio;
+	
+	private Status status;
+	
+	private Cliente cliente;
+
+	
+	public InserirPedidoDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public InserirPedidoDTO(Pedido pedido) {
+		super();
+		this.id = pedido.getId();
+		this.dataPedido = pedido.getDataPedido();
+		this.dataEntrega = pedido.getDataEntrega();
+		this.dataEnvio = pedido.getDataEnvio();
+		this.status = pedido.getStatus();
+		this.cliente = pedido.getCliente();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+
+
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+
+	public LocalDate getDataEnvio() {
+		return dataEnvio;
+	}
+
+
+	public void setDataEnvio(LocalDate dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+
+	public Status getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+}
