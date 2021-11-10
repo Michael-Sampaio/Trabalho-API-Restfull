60cd122207ffe8bd188036263202919460126345
Michael-Sampaio <tradsampaio@gmail.com>
2021-11-10 13:14:33 -0300

    atualização

--- src/main/java/br/org/serratec/backend/controller/EnderecoController.java
+++ src/main/java/br/org/serratec/backend/controller/EnderecoController.java
@@ -1,20 +1,13 @@
 package br.org.serratec.backend.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.backend.dto.EnderecoDTO;
-import br.org.serratec.backend.model.Endereco;
 import br.org.serratec.backend.service.EnderecoService;
 
 @RestController
@@ -34,11 +27,5 @@ public class EnderecoController {
 		return ResponseEntity.ok(enderecoDTO);
 
 	}
-	
-	/*@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public EnderecoDTO inserir(@RequestBody Endereco endereco) {
-		return EnderecoDTO(endereco);
-	}*/
 
 }
