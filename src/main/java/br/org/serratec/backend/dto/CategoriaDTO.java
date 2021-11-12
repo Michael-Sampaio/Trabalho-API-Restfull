b5db18b247fdf6a9d94ebab9c184b8ddd473c552
camillaslp <slpcamilla@gmail.com>
2021-11-12 14:30:56 -0300

    categoriadto

---
+++ src/main/java/br/org/serratec/backend/dto/CategoriaDTO.java
@@ -0,0 +1,45 @@
+package br.org.serratec.backend.dto;
+
+import javax.persistence.Embeddable;
+
+import br.org.serratec.backend.model.Categoria;
+
+@Embeddable
+public class CategoriaDTO {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+
+	public CategoriaDTO() {
+		super();
+	}
+
+	public CategoriaDTO(Categoria categoria) {
+		super();
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+		this.descricao = categoria.getDescricao();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
